2791d45269c944fb0b66693509517b16b09737e3
GhostProtocol <91773656+SUSPECT-911@users.noreply.github.com>
2026-02-08 10:50:28 +0530

    Sample Website Design 2 for Beginners

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelector(".btn-hero").onclick = () =>
+  alert("Projects coming soon!");
+
+document.querySelector(".btn-outline").onclick = () =>
+  alert("Login page");
+
+document.querySelector(".btn-filled").onclick = () =>
+  alert("Signup page");
